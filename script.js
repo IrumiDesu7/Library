da1005c5a7e40426b6c89ba400a0b56032bd66e2
IrumiDesu7 <ikalam89@gmail.com>
2022-04-29 08:25:19 +0800

    added function to add book array and display it

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const bookTable = document.querySelector('table');
+const tableBody = document.getElementById('tableBody');
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -15,4 +18,28 @@ function Book(title, author, pages, read) {
   };
 }
 
-function addBookToLibrary() {}
+function addBook(obj) {
+  myLibrary.push(obj);
+}
+
+const book1 = new Book('Matematika itu Menyenangkan', 'Ilmi Kalam', 333, true);
+const book2 = new Book('Fisika itu Susah', 'SopiCOD', 132, false);
+
+addBook(book1);
+addBook(book2);
+
+function displayItem(arr) {
+  let books = '';
+  arr.forEach((item) => {
+    books += `<tr>
+          <th>${item.title}</th>
+          <th>${item.author}</th>
+          <th>${item.pages}</th>
+          <th>${item.read}</th>
+        </tr>`;
+    tableBody.innerHTML = books;
+    bookTable.appendChild(tableBody);
+  });
+}
+
+displayItem(myLibrary);
